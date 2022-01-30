56d34aad889e74dddd89cd45457811dbc95edf4d
Tomek Michalski <tmm.michalski@gmail.com>
2022-01-30 12:31:21 +0100

    define cli color scheme

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+
+CLI_COLORS = {
+    'HDR': '\033[95m',
+    'OKB': '\033[94m',
+    'OKC': '\033[96m',
+    'OKG': '\033[92m',
+    'WRN': '\033[31m',
+    'CMT': '\033[93m',
+    'FAIL': '\033[91m',
+    'ENDC': '\033[0m',
+    'BLD': '\033[1m',
+    'UND': '\033[4m',
+}
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
